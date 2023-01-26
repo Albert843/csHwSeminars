98700b0c3f5fd8762a3ae1533add746f61fc9688
Albert843 <nuralbert@yandex.ru>
2023-01-26 21:54:45 +0300

    Home work for Seminar7

--- Program.cs
+++ Program.cs
@@ -383,3 +383,125 @@ else{
 // }
 
 
+//=================================================================================================================================
+
+//                                          Урок 7 (Семинар 7)
+
+//                                             Задача 47:
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами. m=3, n=4.
+
+// double[,] array = GetArray(3, 4, -10, 10);
+// PrintArray(array);
+
+// //-----Methods-----
+// double[,] GetArray(int m, int n, int minValue, int maxValue){
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+//         }
+//     }
+//     return result;
+// }   
+// void PrintArray(double[,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]:f1} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+//                                              Задача 50: 
+// Напишите программу, которая на вход принимает число, проверяя есть ли такое число в двумерном массиве 
+// и возвращает сообщение о том, что оно найдено или же указание, что такого элемента нет.
+
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine()!);
+// int[,] array = GetArray(3, 4, 1, 10);
+// PrintArray(array);
+// if(FindNumber(array, num)) Console.WriteLine($"{num} => такое число в массиве есть!");
+// else Console.WriteLine($"{num} => такого числа в массиве нет");
+
+// //-----Methods-----
+// int[,] GetArray(int m, int n, int minValue, int maxValue){
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue);
+//         }
+//     }
+//     return result;
+// }
+// void PrintArray(int[,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// bool FindNumber(int[,] array, int number){
+//     bool isPresent = false;
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(array[i, j] == number){
+//                 isPresent = true;
+//             }
+//         }
+//     }
+//     return isPresent;
+// }
+
+//                                               Задача 52:
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// int[,] array = GetArray(3, 3, 1, 10);
+// PrintArray(array);
+// Average(array);
+// //-----Methods-----
+// int[,] GetArray(int m, int n, int minValue, int maxValue){
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue);
+//         }
+//     }
+//     return result;
+// }
+// void PrintArray(int[,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]}   ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// void Average(int[,] array){
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         int sum = 0;
+//         double result = Convert.ToDouble(sum);
+//         double count = 0;
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             sum += array[j, i];
+//             count++;
+//         }
+//         Console.Write($"{(sum / count):f1} ");
+//     }
+// }
